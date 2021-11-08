c4039d41501e4dbe4e4c92ec53cbb44ceb9d3317
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2021-11-07 21:22:22 -0600

    Added Styling to Control Panel

---
+++ main.js
@@ -0,0 +1,17 @@
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+function populateLetterPanel() {
+    let letterPanel = document.querySelector(".letter-panel");
+    for (let char of ALPHABET) {
+        let letter = document.createElement("button");
+        letter.setAttribute("class", "letter-panel__button");
+        letter.innerText = char;
+        letterPanel.appendChild(letter);
+    }
+}
+
+document.querySelector("#guessForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+populateLetterPanel();
